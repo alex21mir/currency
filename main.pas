f4e33529b30970f53666cd356db9ff1dbf239ba9
alex21mir <alkkpi@gmail.com>
2024-04-15 20:04:19 +0300

    add load rates from NBU

--- main.pas
+++ main.pas
@@ -7,7 +7,8 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects, FMX.Layouts, FMX.ListBox, FMX.Controls.Presentation, FMX.Edit, FMX.ListView.Types, FMX.ListView.Appearances,
   FMX.ListView.Adapters.Base, FMX.ListView, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
   FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.FMXUI.Wait, Data.DB, FireDAC.Comp.Client, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
-  FireDAC.DApt, FireDAC.Comp.DataSet, System.Rtti, System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt, Fmx.Bind.DBEngExt, Data.Bind.Components, Data.Bind.DBScope, FMX.Effects, FMX.StdCtrls;
+  FireDAC.DApt, FireDAC.Comp.DataSet, System.Rtti, System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt, Fmx.Bind.DBEngExt, Data.Bind.Components, Data.Bind.DBScope, FMX.Effects, FMX.StdCtrls, FMX.Ani,
+  System.Net.URLClient, System.Net.HttpClient, System.Net.HttpClientComponent,System.JSON;
 
 type
   TfrmMain = class(TForm)
@@ -36,14 +37,23 @@ type
     ShadowEffect2: TShadowEffect;
     Button1: TButton;
     Button2: TButton;
+    Image1: TImage;
+    qExec: TFDQuery;
+    FDTransaction1: TFDTransaction;
+    Timer1: TTimer;
     procedure Edit1Change(Sender: TObject);
     procedure ComboBox1Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure ListView1DeletingItem(Sender: TObject; AIndex: Integer; var ACanDelete: Boolean);
     procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
+    procedure FDConnection1BeforeConnect(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
     procedure SetErrorText(txt:string);
     procedure LogExcept(code,errtext,msg:string);
+    procedure UpdateRate(isAll:boolean);
   public
     { Public declarations }
   end;
@@ -56,6 +66,8 @@ implementation
 {$R *.fmx}
 
 uses  System.IOUtils;
+
+
 
 procedure TfrmMain.Button2Click(Sender: TObject);
 begin
@@ -91,11 +103,16 @@ begin
      end;
 end;
 
-procedure TfrmMain.FormCreate(Sender: TObject);
+procedure TfrmMain.FDConnection1BeforeConnect(Sender: TObject);
 begin
 {$IF DEFINED(iOS) or DEFINED(ANDROID)}
   FDConnection1.Params.Values['Database']:=TPath.Combine(TPath.GetDocumentsPath, 'rates.s3db');
 {$ENDIF}
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+
   rdelete.Visible:=false; // hide form for delete ask
   SetErrorText('');       // Clear error message
   try
@@ -107,13 +124,18 @@ begin
   end;
 end;
 
+
+
 procedure TfrmMain.ListView1DeletingItem(Sender: TObject; AIndex: Integer; var ACanDelete: Boolean);
 var ccy:string;
 begin
   ccy:=ListView1.Items[AIndex].Text.Split([' '])[0];
-  lblAsk.Text:='Confirm to delete the cyrrency: '+ccy;
-  rDelete.Align:=TAlignLayout.Client;
-  rDelete.Visible:=true;
+  if ccy<>'UAH' then begin  // UAH is important currency for calculate rate
+    lblAsk.Text:='Confirm to delete the cyrrency: '+ccy;
+    lblAsk.TagString:=ccy;
+    rDelete.Align:=TAlignLayout.Client;
+    rDelete.Visible:=true;
+  end;
   ACanDelete:=false;
 end;
 
@@ -148,5 +170,93 @@ begin
       ListView1.Visible:=false;
   end;
 end;
+
+
+procedure TfrmMain.Timer1Timer(Sender: TObject);
+begin
+// for real-time display rate!
+Timer1.Enabled:=false;
+  UpdateRate(false);
+timer1.Enabled:=true;
+end;
+
+
+
+procedure TfrmMain.Button1Click(Sender: TObject);
+begin
+  try
+    qCcy.close;
+    qData.close;
+  FDTransaction1.StartTransaction;
+    qExec.ExecSQL('delete from rates where ccy= :CCY',[lblAsk.TagString]);
+  FDTransaction1.commit;
+    qCcy.Open;
+    qData.Open;
+  except on e:exception do begin
+    if FDTransaction1.Active then FDTransaction1.Rollback;
+     LogExcept('DELETE',e.Message,'Error on delete currency');
+  end;
+  end;
+  rDelete.Visible:=false;
+end;
+
+
+procedure TfrmMain.Image1Click(Sender: TObject);
+begin
+  updateRate(true);
+end;
+
+
+
+
+procedure TfrmMain.UpdateRate(isAll: boolean);
+var
+  HTTP:TnetHTTPClient;
+  rs, ccy:string;
+  rate:extended;
+  JSON:TJSONArray;
+  x:integer;
+begin
+  qCcy.close;
+  qData.close;
+  FDTransaction1.StartTransaction;
+  try
+    if isall then qExec.ExecSQL('delete from rates where ccy<> ''UAH'''); // clear all rates
+     HTTP := TnetHTTPClient.Create(nil);
+      try
+        HTTP.ConnectionTimeout := 1000;
+        HTTP.ResponseTimeout := 5000;
+        http.UserAgent:='Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36';
+        // Get date from NBU
+
+          rs:=HTTP.Get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json').ContentAsString;
+          try
+            JSON:=TJSONObject.ParseJSONValue(rs) as TJSONArray;
+            for x:=0 to JSON.Count -1 do begin
+             ccy:=(json.Items[x] as TJSONObject).GetValue('cc').Value;
+             rate:=StrToFloatDef((json.Items[x] as TJSONObject).GetValue('rate').value.Replace('.',','),0);
+             if isall then qExec.ExecSQL('insert into rates (ccy,rate) values(:CCY, :RATE)',[ccy,rate]) else
+                           qExec.ExecSQL('update rates set rate= :RATE where ccy =:CCY',[ccy,rate]);
+            end;
+           except on e:exception do begin
+            if FDTransaction1.Active then FDTransaction1.Rollback;
+            LogExcept('RESPONCE',e.Message,'Server is not available');
+           end;
+          end;
+
+        if FDTransaction1.Active then FDTransaction1.commit;
+      finally
+        HTTP.Free;
+      end;
+    qCcy.Open;
+    qData.Open;
+  except on e:exception do  begin
+     if FDTransaction1.Active then FDTransaction1.Rollback;
+     LogExcept('UPDATE',e.Message,'Error on update rates');
+  end;
+  end;
+end;
+
+
 
 end.
